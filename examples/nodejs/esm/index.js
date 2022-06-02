c0744cbd8d76d68faaf53144dbc36d03bc9d0208
Tushar <tushar@100ms.live>
2022-06-03 05:03:52 +0530

    feat: increase poll timeout in examples

--- examples/nodejs/esm/index.js
+++ examples/nodejs/esm/index.js
@@ -7,15 +7,21 @@ const transport = sdk.getTransportService();
 // start hls and get m3u8 url back
 const hlsIdentifier = "referenceId"; // use this to call stop
 const appUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-const { url: urlWithStart } = await transport.startHLSAndGetUrl({
-  identifier: hlsIdentifier,
-  appUrl,
-});
-console.log(`started hls for appUrl - ${appUrl} at hls url - ${urlWithStart}`);
+try {
+  const { url: urlWithStart } = await transport.startHLSAndGetUrl({
+    identifier: hlsIdentifier,
+    appUrl,
+  });
+  console.log(
+    `started hls for appUrl - ${appUrl} at hls url - ${urlWithStart}`
+  );
 
-const { url: urlLater } = await transport.getHlsState({
-  identifier: hlsIdentifier,
-});
-console.log("hls url retrieved later", urlLater);
+  const { url: urlLater } = await transport.getHlsState({
+    identifier: hlsIdentifier,
+  });
+  console.log("hls url retrieved later", urlLater);
+} catch (err) {
+  console.error("failed to start hls and get url ", err);
+}
 
 await transport.stopHLS({ identifier: hlsIdentifier });
