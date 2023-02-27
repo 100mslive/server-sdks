2d53bfddb9e974d160b9c628438b89af85d668ff
Karthikeyan S <karthikeyanssvk@gmail.com>
2023-02-27 07:05:27 +0530

    format with prettier

--- nodejs/src/apis/RoomAPIs.ts
+++ nodejs/src/apis/RoomAPIs.ts
@@ -4,52 +4,55 @@ import { QueryResults } from "./interfaces/common";
 import { HMSCreateRoomConfig, HMSRoom, HMSUpdateRoomConfig } from "./interfaces/roomInterfaces";
 
 export class RoomAPIs {
-    private basePath = "/rooms";
+  private basePath = "/rooms";
 
-    constructor(private apiService: APIService) { }
+  constructor(private apiService: APIService) {}
 
-    async getRooms(): Promise<HMSRoom[]> {
-        const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath);
-        // atleast 1 room(default) will be present
-        return results.data!;
+  async getRooms(): Promise<HMSRoom[]> {
+    const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath);
+    // atleast 1 room(default) will be present
+    return results.data!;
+  }
+
+  async getRoomById(roomId: string): Promise<HMSRoom> {
+    return this.apiService.get(`${this.basePath}/${roomId}`);
+  }
+
+  async getRoomByName(name: string): Promise<HMSRoom> {
+    const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath, { name });
+    if (!results.data || results.data.length === 0) {
+      const err = new Error(`no room found with passed in name - ${name}`);
+      logger.error("no room found", err);
+      throw err;
     }
+    return results.data[0];
+  }
 
-    async getRoomById(roomId: string): Promise<HMSRoom> {
-        return this.apiService.get(`${this.basePath}/${roomId}`);
-    }
+  async getRoomsByEnabled(enabled: boolean): Promise<HMSRoom[]> {
+    const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath, { enabled });
+    return results.data ?? [];
+  }
 
-    async getRoomByName(name: string): Promise<HMSRoom> {
-        const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath, { name });
-        if (!results.data || results.data.length === 0) {
-            const err = new Error(`no room found with passed in name - ${name}`);
-            logger.error("no room found", err);
-            throw err;
-        }
-        return results.data[0];
-    }
+  async getRoomsByTimeRange(before?: Date, after?: Date): Promise<HMSRoom[]> {
+    const timeQueryParams: Record<string, Date> = {};
+    if (before) timeQueryParams["before"] = before;
+    if (after) timeQueryParams["after"] = after;
+    const results: QueryResults<HMSRoom> = await this.apiService.get(
+      this.basePath,
+      timeQueryParams
+    );
+    return results.data ?? [];
+  }
 
-    async getRoomsByEnabled(enabled: boolean): Promise<HMSRoom[]> {
-        const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath, { enabled });
-        return results.data ?? [];
-    }
+  async createRoom(roomConfig: HMSCreateRoomConfig): Promise<HMSRoom> {
+    return this.apiService.post(this.basePath, roomConfig);
+  }
 
-    async getRoomsByTimeRange(before?: Date, after?: Date): Promise<HMSRoom[]> {
-        const timeQueryParams: Record<string, Date> = {};
-        if (before) timeQueryParams['before'] = before;
-        if (after) timeQueryParams['after'] = after;
-        const results: QueryResults<HMSRoom> = await this.apiService.get(this.basePath, timeQueryParams);
-        return results.data ?? [];
-    }
+  async updateRoom(roomConfig: HMSUpdateRoomConfig): Promise<HMSRoom> {
+    return this.apiService.post(this.basePath, roomConfig);
+  }
 
-    async createRoom(roomConfig: HMSCreateRoomConfig): Promise<HMSRoom> {
-        return this.apiService.post(this.basePath, roomConfig);
-    }
-
-    async updateRoom(roomConfig: HMSUpdateRoomConfig): Promise<HMSRoom> {
-        return this.apiService.post(this.basePath, roomConfig);
-    }
-
-    async enableOrDisableRoom(roomId:string, enabled: boolean): Promise<HMSRoom>  { 
-        return this.apiService.post(`${this.basePath}/${roomId}`, {enabled});
-    }
+  async enableOrDisableRoom(roomId: string, enabled: boolean): Promise<HMSRoom> {
+    return this.apiService.post(`${this.basePath}/${roomId}`, { enabled });
+  }
 }
--- nodejs/src/apis/interfaces/roomInterfaces.ts
+++ nodejs/src/apis/interfaces/roomInterfaces.ts
@@ -1,28 +1,28 @@
 export interface HMSRoom {
-  id: string
-  name: string
-  enabled: boolean
-  description: string
-  customer_id: string
-  app_id: string
-  // not sure about the next two
-  recording_source_template?: boolean
-  enabled_source_template?: boolean
-  recording_info?: RecordingInfo
-  template_id: string
-  template: string
-  region: string
-  created_at: string
-  updated_at: string
-  customer: string
-}
-
-export interface RecordingInfo {
+  id: string;
+  name: string;
   enabled: boolean;
-  upload_info?: RecordingUploadInfo;
+  description: string;
+  customer_id: string;
+  app_id: string;
+  // not sure about the next two
+  recording_source_template?: boolean;
+  enabled_source_template?: boolean;
+  recording_info?: HMSRoomRecordingInfo;
+  template_id: string;
+  template: string;
+  region: string;
+  created_at: string;
+  updated_at: string;
+  customer: string;
 }
 
-export interface RecordingUploadInfo {
+export interface HMSRoomRecordingInfo {
+  enabled: boolean;
+  upload_info?: HMSRoomRecordingUploadInfo;
+}
+
+export interface HMSRoomRecordingUploadInfo {
   type: string;
   location: string;
   prefix?: string;
@@ -35,6 +35,8 @@ export interface RecordingUploadInfo {
   };
 }
 
-export type HMSCreateRoomConfig = Partial<Pick<HMSRoom, 'name' | 'description' | 'template_id' | 'recording_info' | 'region'>>
+export type HMSCreateRoomConfig = Partial<
+  Pick<HMSRoom, "name" | "description" | "template_id" | "recording_info" | "region">
+>;
 
-export type HMSUpdateRoomConfig = Omit<HMSCreateRoomConfig, 'template_id'>
+export type HMSUpdateRoomConfig = Omit<HMSCreateRoomConfig, "template_id">;
